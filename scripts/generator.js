5804f1fd0697791526795c51ee19ad8c99081306
CoderFrish <1573880184@qq.com>
2026-07-19 22:22:15 +0800

    (feat)(rewrote): 修复部分bug，修改部分

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,6 +1,11 @@
-hexo.extend.generator.register("tags", function () {
-    return {
-        path: "tags.html",
-        layout: ["tags"]
-    }
-})
+// hexo.extend.generator.register("tags", function () {
+//     return {
+//         path: "tags.html",
+//         layout: ["tags"],
+//         data: {
+//             title: "标签"
+//         }
+//     }
+// })
+
+// `hexo-generator-json-content`
