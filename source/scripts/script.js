7b00f8dfba5afe021a1698fca50301772c0e0b60
CoderFrish <1573880184@qq.com>
2026-07-20 10:56:24 +0800

    (feat/fixed)(rewrote): 修复了post页与about页关于标题在暗色模式的情况下为黑色字的问题，添加了保存主题功能

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,11 +1,13 @@
+const html = document.documentElement
+
 function switch_theme() {
-    const html = document.documentElement
     const theme = html.getAttribute("data-theme")
     const theme_btn_icon = document.getElementById("theme_btn_icon")
     const next_theme = theme === "light"? "dark" : "light";
     const next_theme_btn_icon = theme === "light"? "basil:moon-outline": "tabler:sun"
     theme_btn_icon.setAttribute("icon", next_theme_btn_icon)
     html.setAttribute("data-theme", next_theme)
+    window.localStorage.setItem("data-theme", next_theme);
 }
 
 let collapse = false
@@ -22,4 +24,23 @@ function open_mobile_nav() {
         collapse = false
     }
 
+}
+
+function load_theme() {
+    const theme = window.localStorage.getItem("data-theme");
+
+    if (theme) {
+        html.setAttribute("data-theme", theme);
+    }
+}
+
+/* 脚本的主入口 */
+function main() {
+    load_theme()
+}
+
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", main);
+} else {
+    main();
 }
